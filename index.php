8dc98c5ab921cadd9aac52342f3a9a6df1bdbd2e
JCMMelton <JCMM@Chalmerss-MacBook-Pro.local>
2013-11-09 20:04:54 -0800

    adding stuff

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf8'>
+<title> Choosing how you age </title>
+<link rel='stylesheet' type='text/css' href='/green_purple.css'>
+</head>
+<body>
+	<div id='wrapper'>
+		<div id='header'>
+			
+		</div>
+
+		<h1>Choosing How You Age</h1>
+
+		<div id='content'>
+
+
+
+		</div>
+	</div>
+</body>
+</html>
+
+
+<!--    #440e62 #669801    -->
